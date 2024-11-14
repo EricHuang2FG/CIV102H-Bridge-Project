c6ba6dbfcc5c6f434ee3e2bce105b4369cd0d40c
Eric Huang <xilahuade@foxmail.com>
2024-11-13 20:33:01 -0500

    Adding initial code for calculating cross-sectional properties

---
+++ cross_section_calculator.py
@@ -0,0 +1,215 @@
+import pygame
+
+pygame.init()
+WINDOW = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Cross-Section")
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SILVER = (192, 192, 192)
+LIGHT_GRAY = (220, 220, 220)
+DARK_GRAY = (128, 128, 128)
+LIGHT_BLUE = (80, 196, 222)
+ORANGE = (255, 99, 71)
+RED = (255, 0, 0)
+INPUT_FONT = pygame.font.SysFont("couriernew", 16, False)
+INFORMATION_DISPLAY_FONT = pygame.font.SysFont("couriernew", 18, True)
+OFFSET_X = 550
+OFFSET_Y = 150
+
+class InputBox:
+
+    def __init__(self, x, y, description, width=150, height=25):
+        self.x = x
+        self.y = y
+        self.colour = SILVER
+        self.description = INPUT_FONT.render(description, 0, BLACK)
+        self.width = width
+        self.height = height
+        self.active = False
+        self.object = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.value = ""
+    
+    def set_validity(self, is_valid):
+        if not is_valid:
+            self.colour = RED
+        else:
+            self.colour = SILVER
+
+    def draw(self):
+        WINDOW.blit(self.description, (self.x, self.y - 25))
+        pygame.draw.rect(WINDOW, self.colour, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(WINDOW, BLACK, (self.x, self.y, self.width, self.height), 1) # draws an outline
+        text = INPUT_FONT.render(self.value, 0, BLACK)
+        WINDOW.blit(text, (self.x + 2, self.y + (self.height // 2) - (text.get_height() // 2)))
+    
+    def behave(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.object.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.colour = LIGHT_GRAY if self.active else SILVER
+        elif event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    self.value = ""  # Clear the input after pressing Enter
+                elif event.key == pygame.K_BACKSPACE:
+                    self.value = self.value[:-1]  # Remove the last character
+                else:
+                    self.value += event.unicode  # Add the typed character to the input text
+
+class Button:
+
+    def __init__(self, x, y, description, width=125, height=40, colour=LIGHT_BLUE):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.description = INPUT_FONT.render(description, 0, BLACK)
+        self.colour = colour
+        self.object = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.clicked = False
+    
+    def draw(self):
+        pygame.draw.rect(WINDOW, self.colour, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(WINDOW, BLACK, (self.x, self.y, self.width, self.height), 1) # draws an outline
+        WINDOW.blit(self.description, (self.x + (self.width // 2) - (self.description.get_width() // 2), self.y + (self.height // 2) - (self.description.get_height() // 2)))
+
+    def is_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.object.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                return True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        return False
+
+class CrossSection:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.components = []
+        self.display_components = []
+        self.global_min = 0
+        self.second_moment_of_area = 0
+        self.centroidal_axis = 0
+    
+    def draw(self):
+        for object in self.display_components:
+            pygame.draw.rect(WINDOW, DARK_GRAY, object)
+            pygame.draw.rect(WINDOW, BLACK, object, 1)
+        centroidal_axis_display = INFORMATION_DISPLAY_FONT.render(f"y = {self.centroidal_axis} mm", 0, ORANGE)
+        second_moment_of_area_display = INFORMATION_DISPLAY_FONT.render(f"I = {self.second_moment_of_area} mm^4", 0, ORANGE)
+        WINDOW.blit(centroidal_axis_display, (70, 600))
+        WINDOW.blit(second_moment_of_area_display, (70, 635))
+    
+    def behave(self):
+        # find the global minimum (visually lowest, although technically it's the largest)
+        self.global_min = 0
+        for object in self.components:
+            x, y, w, h = object[1]
+            self.global_min = max(self.global_min, y + h)
+        
+        sum_area_y, sum_area = 0, 0
+        for object in self.components:
+            x, y, w, h = object[1]
+            area = w * h
+            local_y = abs(y + (h / 2) - self.global_min)
+            sum_area += area
+            sum_area_y += area * local_y
+        if sum_area != 0:
+            self.centroidal_axis = sum_area_y / sum_area
+        print(f"y: {self.centroidal_axis}, sum_area: {sum_area}, sum_area_y: {sum_area_y}")
+
+        self.second_moment_of_area = 0
+        for object in self.components:
+            x, y, w, h = object[1]
+            area = w * h
+            self.second_moment_of_area += (area * (y + (h / 2) - (self.global_min - self.centroidal_axis)) ** 2) + (w * (h ** 3)) / 12
+        print(f"I: {self.second_moment_of_area}")
+
+x_box = InputBox(70, 100, "Input the x coordinate (top left): ")
+y_box = InputBox(70, 175, "Input the y coordinate (top left): ")
+width_box = InputBox(70, 250, "Input the width: ")
+height_box = InputBox(70, 325, "Input the height: ")
+input_fields = [x_box, y_box, width_box, height_box]
+add_object_button = Button(70, 400, "Add")
+
+cross_section = CrossSection()
+
+def overlaps(coords1, coords2) -> bool:
+    x1, y1, w1, h1 = coords1
+    x2, y2, w2, h2 = coords2
+    left1, right1 = x1, x1 + w1
+    top1, bottom1 = y1, y1 + h1
+    left2, right2 = x2, x2 + w2
+    top2, bottom2 = y2, y2 + h2
+
+    return not (
+        right1 <= left2 or
+        left1 >= right2 or
+        bottom1 <= top2 or
+        top1 >= bottom2
+    )
+
+def draw_background() -> None:
+    WINDOW.fill(WHITE)
+
+def draw_elements() -> None:
+    draw_background()
+    for field in input_fields:
+        field.draw()
+    add_object_button.draw()
+    cross_section.draw()
+
+def behave_elements(event) -> None:
+    for field in input_fields:
+        field.behave(event)
+    cross_section.behave()
+
+def main() -> None:
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            behave_elements(event)
+
+        if add_object_button.is_clicked():
+            try:
+                for field in input_fields:
+                    float(field.value)      # check if it's a float. If not, it will go to the exception block
+
+                # create a new rectangle with an offset to the coordinate
+                new_rectangle = pygame.Rect(OFFSET_X + float(x_box.value), OFFSET_Y + float(y_box.value), float(width_box.value), float(height_box.value))
+                display_rectangle = pygame.Rect(OFFSET_X + 5 * float(x_box.value), OFFSET_Y + 5 * float(y_box.value), 5 * float(width_box.value), 5 * float(height_box.value))
+                for object in cross_section.components:
+                    if overlaps((OFFSET_X + float(x_box.value), OFFSET_Y + float(y_box.value), float(width_box.value), float(height_box.value)), object[1]): # check if it's overlapping with other objects which is bad
+                        for field in input_fields:
+                            field.set_validity(False)
+                        break
+                else:
+                    cross_section.components.append((new_rectangle, (float(x_box.value), float(y_box.value), float(width_box.value), float(height_box.value))))
+                    cross_section.display_components.append(display_rectangle)
+                    for field in input_fields:
+                        field.value = ""
+            except Exception as e:
+                for field in input_fields:
+                    field.set_validity(False)
+                print(e)
+
+        draw_elements()
+
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()

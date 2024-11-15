eab345170cc880bc0cf11a72a5640b6cd4bd7720
Eric Huang <xilahuade@foxmail.com>
2024-11-15 17:38:27 -0500

    Added stress calculations. PLEASE HELP TEST, THANKSgit add .

--- cross_section_calculator.py
+++ cross_section_calculator.py
@@ -95,8 +95,11 @@ class CrossSection:
         self.components = []
         self.display_components = []
         self.global_min = 0
+        self.global_max = 0
         self.second_moment_of_area = 0
         self.centroidal_axis = 0
+        self.top_stress = 0
+        self.bottom_stress = 0
     
     def draw(self):
         for object in self.display_components:
@@ -104,10 +107,14 @@ class CrossSection:
             pygame.draw.rect(WINDOW, BLACK, object, 1)
         centroidal_axis_display = INFORMATION_DISPLAY_FONT.render(f"y = {self.centroidal_axis} mm", 0, ORANGE)
         second_moment_of_area_display = INFORMATION_DISPLAY_FONT.render(f"I = {self.second_moment_of_area} mm^4", 0, ORANGE)
+        top_stress_display = INFORMATION_DISPLAY_FONT.render(f"Top stress: {self.top_stress} MPa", 0, ORANGE)
+        bottom_stress_display = INFORMATION_DISPLAY_FONT.render(f"Bottom stress: {self.bottom_stress} MPa", 0, ORANGE)
         WINDOW.blit(centroidal_axis_display, (70, 600))
         WINDOW.blit(second_moment_of_area_display, (70, 635))
+        WINDOW.blit(top_stress_display, (70, 670))
+        WINDOW.blit(bottom_stress_display, (70, 705))
     
-    def behave(self, event):
+    def behave(self, event, moment):
         if event.type == pygame.MOUSEBUTTONDOWN:
             for index, object in enumerate(self.display_components):
                 if object.collidepoint(event.pos):
@@ -141,11 +148,23 @@ class CrossSection:
             self.second_moment_of_area += (area * (y + (h / 2) - (self.global_min - self.centroidal_axis)) ** 2) + (w * (h ** 3)) / 12
         print(f"I: {self.second_moment_of_area}")
 
+        if moment:
+            try:
+                float(moment) # if this fails, it will be caught by the exception block
+                self.top_stress = abs(float(moment)) * (self.global_min - self.centroidal_axis) / self.second_moment_of_area
+                self.bottom_stress = abs(float(moment)) * self.centroidal_axis / self.second_moment_of_area
+            except Exception as e:
+                print(e)
+                moment_box.set_validity(False)
+        else:
+            self.top_stress, self.bottom_stress = 0, 0
+
 x_box = InputBox(70, 100, "Input the x coordinate (top left): ")
 y_box = InputBox(70, 175, "Input the y coordinate (top left): ")
 width_box = InputBox(70, 250, "Input the width: ")
 height_box = InputBox(70, 325, "Input the height: ")
-input_fields = [x_box, y_box, width_box, height_box]
+moment_box = InputBox(70, 500, "Input the max moment (negative for top tension): ")
+input_fields = [x_box, y_box, width_box, height_box, moment_box]
 add_object_button = Button(70, 400, "Add")
 
 cross_section = CrossSection()
@@ -175,10 +194,10 @@ def draw_elements() -> None:
     add_object_button.draw()
     cross_section.draw()
 
-def behave_elements(event) -> None:
+def behave_elements(event, moment) -> None:
     for field in input_fields:
         field.behave(event)
-    cross_section.behave(event)
+    cross_section.behave(event, moment)
 
 def main() -> None:
     clock = pygame.time.Clock()
@@ -190,12 +209,13 @@ def main() -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            behave_elements(event)
+            behave_elements(event, moment_box.value)
 
         if add_object_button.is_clicked():
             try:
-                for field in input_fields:
-                    float(field.value)      # check if it's a float. If not, it will go to the exception block
+                for index, field in enumerate(input_fields):
+                    if index != len(input_fields) - 1:
+                        float(field.value)      # check if it's a float. If not, it will go to the exception block
 
                 # create a new rectangle with an offset to the coordinate
                 new_rectangle = pygame.Rect(OFFSET_X + float(x_box.value), OFFSET_Y + float(y_box.value), float(width_box.value), float(height_box.value))
